0b07d8848d1f743cd66ca6b35f14152eb557c750
Teerapat Aksonsarasit <plovet2032@gmail.com>
2022-04-01 02:12:49 +0700

    fix: get userdata

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const dotenv = require("dotenv");
 // dotenv.config();
 dotenv.config({ path: "./.env" });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
